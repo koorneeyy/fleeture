4afaa3860cd0e00ab60a86de117a5957e70e5197
korney <koorneeyy@gmail.com>
2019-11-02 20:22:21 +0200

    Optioanl task complete!

--- src/de/fleeture/Main.java
+++ src/de/fleeture/Main.java
@@ -8,13 +8,21 @@ public class Main {
 
     public static void main(String[] args){
         File drectory = null;
+        int genre= -1;
         for ( int i = 0; i < args.length; i++ ) {
             if ( args[i].equals("-f") ) {
                 i++;
                 drectory = new File(args[i]);
+            }else if ( args[i].equals("-g") ) {
+                i++;
+                try {
+                    genre = Integer.parseInt(args[i]);
+                }catch (NumberFormatException e){
+                    genre = Util.getGenreByValue(args[i]);
+                }
             }
         }
-        if(drectory==null || !drectory.isDirectory()){
+        if(drectory == null || !drectory.isDirectory()){
             System.err.println("You should specify existing directory after -f parameter key.");
             System.exit(1);
         }
@@ -24,6 +32,13 @@ public class Main {
         List<File> files = mp3Finder.join();
         List<Mp3Pojo> mp3Pojos = TagReader.readTagsFromFiles(files);
 
-        mp3Pojos.forEach(System.out::println);
+        if(Util.isGenreIdCorrect(genre)){
+            int finalGenre = genre;
+            mp3Pojos.stream()
+                    .filter(i -> i.getGenre() == finalGenre)
+                    .forEach(System.out::println);
+        }else {
+            mp3Pojos.forEach(System.out::println);
+        }
     }
 }
--- src/de/fleeture/Util.java
+++ src/de/fleeture/Util.java
@@ -1,11 +1,14 @@
 package de.fleeture;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Util {
 
-    public static final HashMap<Integer,String> GENRES =new HashMap<>();
-   static  {
+    public static final HashMap<Integer, String> GENRES = new HashMap<>();
+
+    static {
         GENRES.put(0, "Blues");
         GENRES.put(1, "Classic rock");
         GENRES.put(2, "Country");
@@ -19,76 +22,90 @@ public class Util {
         GENRES.put(10, "New Age");
         GENRES.put(11, "Oldies");
         GENRES.put(12, "Other");
-        GENRES.put( 13, "Pop");
-        GENRES.put( 14, "Rhythm and Blues");
-        GENRES.put( 15, "Rap");
-        GENRES.put( 16, "Reggae");
-        GENRES.put( 17, "Rock");
-        GENRES.put( 18, "Techno");
-        GENRES.put( 19, "Industrial");
-        GENRES.put( 20, "Alternative");
-        GENRES.put( 21, "Ska");
-        GENRES.put( 22, "Death metal");
-        GENRES.put( 23, "Pranks");
-        GENRES.put( 24, "Soundtrack");
-        GENRES.put( 25, "Euro-Techno");
-        GENRES.put( 26, "Ambient");
-        GENRES.put( 27, "Trip-Hop");
-        GENRES.put( 28, "Vocal");
-        GENRES.put( 29, "Jazz & Funk");
-        GENRES.put( 30, "Fusion");
-        GENRES.put( 31, "Trance");
-        GENRES.put( 32, "Classical");
-        GENRES.put( 33, "Instrumental");
-        GENRES.put( 34, "Acid");
-        GENRES.put( 35, "House");
-        GENRES.put( 36, "Game");
-        GENRES.put( 37, "Sound clip");
-        GENRES.put( 38, "Gospel");
-        GENRES.put( 39, "Noise");
-        GENRES.put( 40, "Alternative Rock");
-        GENRES.put( 41, "Bass");
-        GENRES.put( 42, "Soul");
-        GENRES.put( 43, "Punk");
-        GENRES.put( 44, "Space");
-        GENRES.put( 45, "Meditative");
-        GENRES.put( 46, "Instrumental Pop");
-        GENRES.put( 47, "Instrumental Rock");
-        GENRES.put( 48, "Ethnic");
-        GENRES.put( 49, "Gothic");
-        GENRES.put( 50, "Darkwave");
-        GENRES.put( 51, "Techno-Industrial");
-        GENRES.put( 52, "Electronic");
-        GENRES.put( 53, "Pop-Folk");
-        GENRES.put( 54, "Eurodance");
-        GENRES.put( 55, "Dream");
-        GENRES.put( 56, "Southern Rock");
-        GENRES.put( 57, "Comedy");
-        GENRES.put( 58, "Cult");
-        GENRES.put( 59, "Gangsta");
-        GENRES.put( 60, "Top 40");
-        GENRES.put( 61, "Christian Rap");
-        GENRES.put( 62, "Pop/Funk");
-        GENRES.put( 63, "Jungle");
-        GENRES.put( 64, "Native US");
-        GENRES.put( 65, "Cabaret");
-        GENRES.put( 66, "New Wave");
-        GENRES.put( 67, "Psychedelic");
-        GENRES.put( 68, "Rave");
-        GENRES.put( 69, "Show tunes");
-        GENRES.put( 70, "Trailer");
-        GENRES.put( 71, "Lo-Fi");
-        GENRES.put( 72, "Tribal");
-        GENRES.put( 73, "Acid Punk");
-        GENRES.put( 74, "Acid Jazz");
-        GENRES.put( 75, "Polka");
-        GENRES.put( 76, "Retro");
-        GENRES.put( 77, "Musical");
-        GENRES.put( 78, "Rock ’n’ Roll");
-        GENRES.put( 79, "Hard Rock");
+        GENRES.put(13, "Pop");
+        GENRES.put(14, "Rhythm and Blues");
+        GENRES.put(15, "Rap");
+        GENRES.put(16, "Reggae");
+        GENRES.put(17, "Rock");
+        GENRES.put(18, "Techno");
+        GENRES.put(19, "Industrial");
+        GENRES.put(20, "Alternative");
+        GENRES.put(21, "Ska");
+        GENRES.put(22, "Death metal");
+        GENRES.put(23, "Pranks");
+        GENRES.put(24, "Soundtrack");
+        GENRES.put(25, "Euro-Techno");
+        GENRES.put(26, "Ambient");
+        GENRES.put(27, "Trip-Hop");
+        GENRES.put(28, "Vocal");
+        GENRES.put(29, "Jazz & Funk");
+        GENRES.put(30, "Fusion");
+        GENRES.put(31, "Trance");
+        GENRES.put(32, "Classical");
+        GENRES.put(33, "Instrumental");
+        GENRES.put(34, "Acid");
+        GENRES.put(35, "House");
+        GENRES.put(36, "Game");
+        GENRES.put(37, "Sound clip");
+        GENRES.put(38, "Gospel");
+        GENRES.put(39, "Noise");
+        GENRES.put(40, "Alternative Rock");
+        GENRES.put(41, "Bass");
+        GENRES.put(42, "Soul");
+        GENRES.put(43, "Punk");
+        GENRES.put(44, "Space");
+        GENRES.put(45, "Meditative");
+        GENRES.put(46, "Instrumental Pop");
+        GENRES.put(47, "Instrumental Rock");
+        GENRES.put(48, "Ethnic");
+        GENRES.put(49, "Gothic");
+        GENRES.put(50, "Darkwave");
+        GENRES.put(51, "Techno-Industrial");
+        GENRES.put(52, "Electronic");
+        GENRES.put(53, "Pop-Folk");
+        GENRES.put(54, "Eurodance");
+        GENRES.put(55, "Dream");
+        GENRES.put(56, "Southern Rock");
+        GENRES.put(57, "Comedy");
+        GENRES.put(58, "Cult");
+        GENRES.put(59, "Gangsta");
+        GENRES.put(60, "Top 40");
+        GENRES.put(61, "Christian Rap");
+        GENRES.put(62, "Pop/Funk");
+        GENRES.put(63, "Jungle");
+        GENRES.put(64, "Native US");
+        GENRES.put(65, "Cabaret");
+        GENRES.put(66, "New Wave");
+        GENRES.put(67, "Psychedelic");
+        GENRES.put(68, "Rave");
+        GENRES.put(69, "Show tunes");
+        GENRES.put(70, "Trailer");
+        GENRES.put(71, "Lo-Fi");
+        GENRES.put(72, "Tribal");
+        GENRES.put(73, "Acid Punk");
+        GENRES.put(74, "Acid Jazz");
+        GENRES.put(75, "Polka");
+        GENRES.put(76, "Retro");
+        GENRES.put(77, "Musical");
+        GENRES.put(78, "Rock ’n’ Roll");
+        GENRES.put(79, "Hard Rock");
     }
 
     public static boolean isGenreIdCorrect(byte[] lastBytes) {
         return lastBytes[127] >= 0 && lastBytes[127] < 80;
+    }
+
+    public static boolean isGenreIdCorrect(int genre) {
+        return genre >= 0 && genre < 80;
+    }
+
+    public static int getGenreByValue(String strName) {
+        if (!GENRES.containsValue(strName)) return -1;
+        for (Map.Entry<Integer, String> entry : GENRES.entrySet()) {
+            if (entry.getValue().equals(strName)) {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 }
